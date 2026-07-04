9da5de631ec6e6bba8561c9f37a793caaa9fa6b4
joyprima12 <joyprimasembiring@gmail.com>
2026-07-04 15:58:59 +0700

    Menambahkan fitur back to top button

--- JS/script.js
+++ JS/script.js
@@ -284,4 +284,38 @@ document.addEventListener("DOMContentLoaded", function () {
             btn.addEventListener("click", () => openModalFor(btn));
         });
     }
+
+
+const topBtn = document.createElement("button");
+topBtn.innerHTML = "⬆";
+topBtn.style.position = "fixed";
+topBtn.style.bottom = "80px";
+topBtn.style.right = "20px";
+topBtn.style.width = "45px";
+topBtn.style.height = "45px";
+topBtn.style.border = "none";
+topBtn.style.borderRadius = "50%";
+topBtn.style.background = "#f59e0b";
+topBtn.style.color = "#fff";
+topBtn.style.cursor = "pointer";
+topBtn.style.display = "none";
+topBtn.style.zIndex = "9999";
+
+document.body.appendChild(topBtn);
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
 });
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+});
+//test
